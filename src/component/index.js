54cf5fb2d73d31057113319fff2ea126aa59f735
luckykun <jarson_7426@163.com>
2025-08-04 18:07:25 +0800

    fix

--- src/component/index.js
+++ src/component/index.js
@@ -5,10 +5,15 @@ const Component = () => {
   console.log('data', data);
 
   return (
-    <div style={{ padding: 24, display: 'flex', gap: '8px' }}>
-      <UI.Select style={{ width: 240 }} options={data.data} placeholder="请选择数据" />
-      <UI.Input placeholder="输入名称" />
-      <UI.Button type="primary">搜索</UI.Button>
+    <div>
+      <div style={{ fontSize: 24, textAlign: 'center' }}>
+        Hello, World!
+      </div>
+      <div style={{ padding: 24, display: 'flex', gap: '8px' }}>
+        <UI.Select style={{ width: 240 }} options={data.data} placeholder="请选择数据" />
+        <UI.Input placeholder="输入名称" />
+        <UI.Button type="primary">搜索</UI.Button>
+      </div>
     </div>
   );
 };
